60604e8f65cb20e545ce72f16d3d5fc1d10ae284
Jyurian <ferdian.agustinus@gmail.com>
2018-05-10 13:39:05 +0700

    Second commit !

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React , {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDeJxnA1KQoII57xdAuKiY9stmReCeuG9M';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos:[],
+            selectedVideo : null
+        };
+
+        YTSearch({key: API_KEY, term: 'corgi'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo : videos[0]
+            });
+        });
+    }
+
+    render(){
+    return(
+        <div>
+            <SearchBar />
+            <VideoDetail 
+            onVideoSelect ={selectedVideo => this.setState({selectedVideo})}
+            video={this.state.selectedVideo}/>
+            <VideoList videos={this.state.videos}/>
+        </div>
+        )
+    } 
+}
+
+ReactDOM.render(<App/> , document.querySelector('.container'));
